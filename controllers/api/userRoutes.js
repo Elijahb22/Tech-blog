c8c98b0414ebc17f1bcf59010f27aa8e0e3832d5
elijahb22 <elibusick11@gmail.com>
2022-02-13 16:58:03 -0500

    created a post route for login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-//Create a new user
+//create a new user
 router.post("/", async (req, res) => {
 try {
   const userData = await User.create(req.body);
@@ -15,4 +15,39 @@ try {
 }
 });
 
+// login User
+router.post('/session', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { displayName: req.body.display_name } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
+  
 module.exports = router; 
