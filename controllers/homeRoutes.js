e8c4bcfedd3ed2fb919c7d23f9858fa916bbacdd
elijahb22 <elibusick11@gmail.com>
2022-02-13 20:04:27 -0500

    created get route to get profile and get route to sign up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,22 @@ const router = require('express').Router();
 const { User, Comments, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-
+router.get('/profile', withAuth, async (req, res) => {
+    if (!req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+    try {
+      const user = await User.findByPk(req.session.user_id, {include: "posts"});
+      res.render('profile', {
+        user: user.toJSON(),
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+  
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
       res.redirect('/');
@@ -11,4 +26,10 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/signup', async (req, res) =>{
+    res.render('signup', {
+      logged_in: req.session.logged_in,
+    })
+});
+  
 module.exports = router;
