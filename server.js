b3aa35b9c30d684785759ca3069d2d39635035fb
elijahb22 <elibusick11@gmail.com>
2022-02-13 23:54:21 -0500

    debugged

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ });
 
 const sess = {
     secret: 'Secret Secret Super',
@@ -31,9 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
 
-sequelize.sync({force: false }).then(() => {
+app.use(require('./controllers'));
+
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log('App listening!');
     });
